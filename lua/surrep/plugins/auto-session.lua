7617eb0c2c803f630746aa84b40238892f2e4f35
Surreptitious01 <lawsonewan010@gmail.com>
2024-09-12 21:11:56 -0500

    added autopairs and restore session

---
+++ lua/surrep/plugins/auto-session.lua
@@ -0,0 +1,14 @@
+return {
+  'rmagatti/auto-session',
+  config = function()
+    local auto_session = require("auto-session")
+  
+    auto_session.setup({
+      auto_restore_enabled = false,
+      auto_session_suppress_dirs = { "~", "~/Dev/", "~/Downloads", "~/Documents", "~/Desktop" },
+    })
+
+    vim.keymap.set("n", "<leader>wr", "<cmd>SessionRestore<CR>", {})
+    vim.keymap.set("n", "<leader>ws", "<cmd>SessionSave<CR>", {})
+  end
+}
