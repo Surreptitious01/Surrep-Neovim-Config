23547176480ca1e7496341251a37da911f9ab00b
Surreptitious01 <127454737+Surreptitious01@users.noreply.github.com>
2024-10-12 13:39:21 -0500

    Update none-ls.lua

--- lua/surrep/plugins/none-ls.lua
+++ lua/surrep/plugins/none-ls.lua
@@ -6,8 +6,10 @@ return {
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.clang_format,
-        null_ls.builtins.formatting.biome
+        null_ls.builtins.diagnostics.checkstyle.with({
+          extra_args = { "-c", "/google_checks.xml" }, -- or "/sun_checks.xml" or path to self written rules
+        }),
+        null_ls.builtins.diagnostics.mypy
       },
     })
 
